c78226248b2e6b48b523fe71ad2584631b319551
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-01-22 01:11:20 -0500

    added circle.yml

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# OnboardIQ
+# OnboardIQ [![GitHub version](https://badge.fury.io/gh/stephn-r%2Fonboardiq.svg)](https://badge.fury.io/gh/stephn-r%2Fonboardiq) [![npm version](https://badge.fury.io/js/onboardiq.svg)](https://badge.fury.io/js/onboardiq)
 A node.js client for the OnboardIQ API
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*===============================
+=            MODULES            =
+===============================*/
+
+const request = require('request');
+
+/*=====  End of MODULES  ======*/
+
+const OnboardIQ = module.exports = {};
+
+// OnboardIQ.Client = (client_id)
