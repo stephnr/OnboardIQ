95b2a04839ba817802f5c6ea36256b42827066c7
Stephen Rodriguez <steprodriguez10@gmail.com>
2016-01-23 12:06:21 -0500

    finished v1 api, prepping tests

--- index.js
+++ index.js
@@ -4,10 +4,105 @@
 =            MODULES            =
 ===============================*/
 
-const request = require('request');
+var _ = require('lodash');
+var request = require('request');
+var Promise = require('bluebird');
 
 /*=====  End of MODULES  ======*/
 
-const OnboardIQ = module.exports = {};
+var OnboardIQ = module.exports = {};
 
-// OnboardIQ.Client = (client_id)
+/*===================================
+=            CONSTRUCTOR            =
+===================================*/
+
+OnboardIQ.Client = function(apiToken, env) {
+  if(_.isNil(apiToken)) {
+    throw new Error('Missing OnboardIQ API Token');
+  }
+
+  if(_.isNil(env)) {
+    throw new Error('Missing OnboardIQ API Version');
+  } else if(!env.match(/^(v1|v2)$/)) {
+    throw new Error('Invalid version for OnboardIQ API');
+  }
+
+  this.apiToken = apiToken;
+  this.uri = 'https://www.onboardiq.com/api/' + env;
+};
+
+/*=====  End of CONSTRUCTOR  ======*/
+
+
+/*===============================
+=            METHODS            =
+===============================*/
+
+OnboardIQ.Client.prototype.getApplicant = function(id, params) {
+  var uri = this.uri + '/applicants/' + id;
+  return this._constructPromiseRequest(uri, 'GET', params);
+};
+
+OnboardIQ.Client.prototype.addApplicant = function(params) {
+  var uri = this.uri + '/applicants';
+  return this._constructPromiseRequest(uri, 'POST', params);
+};
+
+OnboardIQ.Client.prototype.updateApplicant = function(id, params) {
+  var uri = this.uri + '/applicants/' + id;
+  return this._constructPromiseRequest(uri, 'PUT', params);
+};
+
+OnboardIQ.Client.prototype.listApplicants = function(params) {
+  var uri = this.uri + '/applicants';
+  return this._constructPromiseRequest(uri, 'GET', params);
+};
+
+OnboardIQ.Client.prototype.deleteApplicant = function(id, params) {
+  var uri = this.uri + '/applicants' + id;
+  return this._constructPromiseRequest(uri, 'DELETE', params);
+};
+
+/*=====  End of METHODS  ======*/
+
+
+/*=======================================
+=            PRIVATE METHODS            =
+=======================================*/
+
+OnboardIQ.Client.prototype._constructPromiseRequest = function(uri, method, params) {
+  var Client = this;
+
+  return new Promise(function(resolve, reject) {
+    Client._authenticatedRequest({
+      uri:    uri,
+      method: method,
+      body:   params
+    }, resolve, reject);
+  });
+};
+
+OnboardIQ.Client.prototype._authenticatedRequest = function(options, resolve, reject) {
+  request({
+    uri:    options.uri,
+    method: options.method,
+    json: _.extend({
+      api_token: this.apiToken
+    }, options.body)
+  }, function(err, res, body) {
+    _handleAPIResponse(err, res, body, resolve, reject);
+  });
+};
+
+function _handleAPIResponse(err, res, body, resolve, reject) {
+  var $body = {};
+
+  if(res) {
+    $body.statusCode = res.statusCode;
+    $body.data = body;
+  }
+
+  return err ? reject(err) : resolve($body);
+}
+
+/*=====  End of PRIVATE METHODS  ======*/
--- test/onboardAPI.js
+++ test/onboardAPI.js
@@ -1,2 +1,25 @@
 'use strict';
 
+var OnboardIQ = require('../index');
+
+var client = new OnboardIQ.Client('5MX5Mf_OEthuf-ecuku7xA', 'v1');
+
+// client.addApplicant({
+//   name: 'Jake',
+//   email: 'jake@gmail.com',
+//   phone_number: '5034834844',
+//   any: 'other',
+//   keys: 'that you might want'
+// }).then(function(status, resp) {
+//   console.log(resp);
+// });
+
+// client.listApplicants().then(function(resp) {
+//   console.log(resp);
+// });
+
+client.updateApplicant('1676b7488094580c', {
+  name: 'Stephen'
+}).then(function(resp) {
+  console.log(resp);
+});
