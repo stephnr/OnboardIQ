c8e7e993e9b42ecd79e84e74b68a8960ed1b0ba7
Cale Kennedy <me@calekennedy.com>
2016-03-23 10:18:27 -0500

    extended tests

--- test/onboardAPI.js
+++ test/onboardAPI.js
@@ -28,11 +28,15 @@ describe('OnboardIQ', function() {
       expect(Client).to.have.property('updateApplicant');
       expect(Client).to.have.property('listApplicants');
       expect(Client).to.have.property('deleteApplicant');
+      expect(Client).to.have.property('listApplicantsLabels');
+      expect(Client).to.have.property('updateApplicantsLabels');
+      expect(Client).to.have.property('listStagesLabels');
     });
   });
 
   describe('Applicants', function() {
     var newApplicant = {};
+    var newLabel = {};
 
     before(function() {
       return Client.addApplicant({
@@ -40,7 +44,8 @@ describe('OnboardIQ', function() {
         email: 'jake@gmail.com',
         phone_number: '5034834844',
         any: 'other',
-        keys: 'that you might want'
+        keys: 'that you might want',
+        data: {state: 'Test'}
       }).then(function(resp) {
         expect(resp.statusCode).to.match(/^20(0|1)$/);
         newApplicant = resp.data;
@@ -80,6 +85,61 @@ describe('OnboardIQ', function() {
         assert.isTrue(found, 'the record exists');
       });
     });
+
+    it('should be able to update an applicants labels', function() {
+      var key = process.env.API_VERSION === 'v2' ? newApplicant.id : newApplicant.key;
+
+      Client.updateApplicantsLabels(key, 'Example Label', true).then(function(resp) {
+        assert.equal(resp.data.title, 'Example Label');
+        newLabel = resp.data;
+      });
+    });
+
+    it('should be able to list an applicants labels', function() {
+      var applicant_id = process.env.API_VERSION === 'v2' ? newApplicant.id : newApplicant.key;
+      var labels, key, found = false;
+      Client.listApplicantsLabels(applicant_id).then(function(resp) {
+        if(process.env.API_VERSION === 'v2') {
+          labels = resp.data.labels;
+          key = 'id';
+        } else {
+          labels = resp.data;
+          key = 'key';
+        }
+
+        labels.forEach(function(el) {
+          if(el[key] === newLabel[key]) {
+            found = true;
+          }
+        });
+
+        assert.isTrue(found, 'the record exists');
+      });
+    });
+
+    // TODO: implement stages tests
+    // it('should be able to list a stages labels', function() {
+    //   var stage_id = process.env.API_VERSION === 'v2' ? newStage.id : newStage.key;
+    //   var labels, key, found = false;
+    //
+    //   Client.listStagesLabels().then(function(resp) {
+    //     if(process.env.API_VERSION === 'v2') {
+    //       labels = resp.data.labels;
+    //       key = 'id';
+    //     } else {
+    //       labels = resp.data;
+    //       key = 'key';
+    //     }
+    //
+    //     labels.forEach(function(el) {
+    //       if(el[key] === newLabel[key]) {
+    //         found = true;
+    //       }
+    //     });
+    //
+    //     assert.isTrue(found, 'the record exists');
+    //   });
+    // });
 
     after(function() {
       var key = process.env.API_VERSION === 'v2' ? 'id' : 'key';
